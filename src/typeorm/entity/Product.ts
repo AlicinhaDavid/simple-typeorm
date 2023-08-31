6cd01656a4062eee6e46dba14193a6bb0a0d37b4
AlicinhaDavid <alicedaviddossantos@gmail.com>
2023-08-31 07:40:23 -0300

    feat: add entity and columns decorators

--- src/typeorm/entity/Product.ts
+++ src/typeorm/entity/Product.ts
@@ -1,7 +1,15 @@
+import { Entity, Column } from "typeorm";
+
+@Entity()
 export class Product {
+  @Column()
   id: string;
+  @Column()
   name: string;
+  @Column()
   description: string;
+  @Column()
   price: number;
+  @Column()
   color: string;
 }
