04f5dc57b05271c6c195fd70ecaa62f25c5fb62b
AlicinhaDavid <alicedaviddossantos@gmail.com>
2023-09-08 12:46:50 -0300

    feat: add create product route

--- src/express/routes/index.ts
+++ src/express/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { Request, Response } from "express";
+import { productRepository } from "../../typeorm/repository/productRepository";
 
 export const routes = Router();
 
@@ -7,3 +8,14 @@ function helloWorld(request: Request, response: Response) {
   return response.json({ message: "Hello World!" });
 }
 routes.get("/", helloWorld);
+
+const createProduct = (request: Request, response: Response) => {
+  const { name, description, price, color } = request.body;
+
+  const product = { name, description, price, color };
+  const productCreated = productRepository().createProduct(product);
+
+  return response.json(productCreated);
+};
+
+routes.post("/createProduct", createProduct);
